3883099e06b3f07a07f02fdfbae92a6120aa66bc
glepter <ingesepulveda@gmail.com>
2023-12-10 13:17:50 -0700

    Add files via upload
    update, changed waiting logic for tick counting logic

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import json
 import dht
 import umqtt.simple as mqtt
 
+#Funcion vinculada al evento de nuevo mensaje en topico suscrito
 def callBack(topic, msg):
     fan = machine.Pin(12, machine.Pin.OUT, machine.Pin.PULL_UP)
     dat = json.dumps(msg).split(':')[-1].split('"')[1]
+    print(dat)
     if str(dat) == "OFF":
         fan.value(1)
     elif str(dat) == "ON":
@@ -16,50 +18,50 @@ def callBack(topic, msg):
         fan.value(1)
 
 
-# Replace these with your own Wi-Fi and MQTT broker settings
-SSID = 'Totalplay-EEA7'
-password = 'EEA7506Es9Jd3Tbj'
-serverAddr = "3.133.119.117"
+#Parametros y etiquetas
+SSID = 'SSID'
+password = 'PASSWORD'
+serverAddr = "192.168.10.102"
 serverPort = 1883
 clientName = 'Bug_01'
 pubTopic = clientName + '/Report'
 subTopic = clientName + '/Execute'
+lastPub = time.ticks_ms() 
+pubInterval = 4000
+sensor = dht.DHT11(machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_UP))
 
 
-# Initialize Wi-Fi
-sta_if = network.WLAN(network.STA_IF)
-sta_if.active(True)
-sta_if.connect(SSID, password)
-while not sta_if.isconnected():
+#Inicializacion de WiFi
+sta = network.WLAN(network.STA_IF)
+sta.active(True)
+sta.connect(SSID, password)
+while not sta.isconnected():
     pass
 
-# Initialize MQTT client
-mqtt_client = mqtt.MQTTClient(clientName, serverAddr, serverPort)
-mqtt_client.connect()
+#Inicializacion del cliente MQTT
+mqttClient = mqtt.MQTTClient(clientName, serverAddr, serverPort)
+mqttClient.connect()
 
-mqtt_client.set_callback(callBack)
-mqtt_client.subscribe(subTopic)
+mqttClient.set_callback(callBack)
+mqttClient.subscribe(subTopic)
 
-sensor = dht.DHT11(machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_UP))
-sensor.
 
+#Programa principal
 while True:
+    #Revisa por mensajes pendientes
+    mqttClient.check_msg()
 
-    try:
-        sensor.measure()
-    
-    except:
-        pass
-    
-    mqtt_client.check_msg()
-    
-     
-    # Create a JSON payload
-    payload = {
-        'temp': sensor.temperature(),
-        'humd': sensor.humidity(),
-    }
-    # Publish the JSON payload to MQTT
-    mqtt_client.publish(pubTopic, json.dumps(payload))
+    #Evalua si ha transcurrido el sampling time, en caso de haberlo hecho, actualiza el tiempo de ultima publicacion
+    if time.ticks_ms() - lastPub >= pubInterval:
+        lastPub = time.ticks_ms()
 
-    time.sleep(4)
+        #Solicita una medicion al DHT11, estructura la informacion en formato json y la publica al topico
+        try:
+            sensor.measure()
+            payload = {
+                'temp': sensor.temperature(),
+                'humd': sensor.humidity(),
+            }
+            mqttClient.publish(pubTopic, json.dumps(payload))
+        except:
+            pass
