1fe7a328b9d27beeb1e0c4885b3bd60cd5ea7ef1
glepter <ingesepulveda@gmail.com>
2023-12-10 13:14:08 -0700

    Add files via upload
    Actualizacion

--- gui.py
+++ gui.py
@@ -5,8 +5,10 @@ from pyowm.owm import OWM
 from pyowm.utils.config import get_default_config_for_subscription_type
 import json
 
+#Se declara la clase
 class MyGUI:
 
+    #Funcion vinculada al evento de nuevo mensaje en topico suscrito
     def on_message(self, mqttClient, userdata, msg):        
         #Discriminacion por topico
         print(msg.payload)
@@ -31,24 +33,25 @@ class MyGUI:
 
                     
 
-
+    #Funcion para limpiar y actualizar datos de los listboxes
     def updateData(self, listbox1, listbox2, dataList):
-        # Clear the Listbox
+        #Limpia el listbox actual
         listbox1.delete(0, tk.END)
         listbox2.delete(0, tk.END)
 
-        # Add the latest messages to the Listbox
+        #Itera sobre los mensajes guardados y los desplega en su listbox
         for message in dataList:
             listbox1.insert(tk.END, message.split(',')[0][1:])
             listbox2.insert(tk.END, message.split(',')[1][:-1])
         
+        #Condicion para confirmar que tengo dato de los dos modulos antes de calcular el promedio para actualizar el label de las condiciones actuales, en caso de no tenerlos actualiza utilizando la informacion del mensaje recien llegado
         if len(self.sensorData1) > 0 and len(self.sensorData2) > 0:
             self.currentAvg = [(float(self.sensorData1[0].split(',')[0].split(':')[-1][1:]) + float(self.sensorData2[0].split(',')[0].split(':')[-1][1:]))/2, (float(self.sensorData1[0].split(',')[1].split(':')[-1][:-1]) + float(self.sensorData2[0].split(',')[1].split(':')[-1][:-1]))/2]
         else :
             self.currentAvg = [message.split(',')[0].split(':')[-1][1:], message.split(',')[1].split(':')[-1][:-1]]
         self.currentParam['text'] = f"Current parameters \n Temperature {self.currentAvg[1]} | Humidity {self.currentAvg[0]}\nDew point: {float(self.currentAvg[1])-((100-float(self.currentAvg[0]))/5)}"
 
-
+    #Funcion vinculada al boton para copiar el clima, obtiene los datos de ciudad y pais, hace la solicitud a OWM actualiza los parametros en la GUI y comienza la barra de carga
     def onSelect(self):
         location = str(self.cityMenu.get())+","+str(self.countryMenu.get())
         observation = mgr.weather_at_place(location)
@@ -58,7 +61,7 @@ class MyGUI:
         self.pb.start()
         self.statusLabel['text'] = 'Copying climate'
         
-
+    #Funcion vinculada a la seleccion del pais, abre el archivo json de las ciudad de OWM, actualiza la lista de ciudades del menu
     def countryOption(self, value):
         with open('city.list.json', 'r', encoding='utf-8') as file:
             data = json.load(file)
@@ -73,120 +76,127 @@ class MyGUI:
         self.cityMenu['values'] = cities
 
         
-
+    #Inicializacion del objeto
     def __init__(self, root):
         self.root = root
         self.root.title("MicroClimate Digital Twin")
 
+        #Creacion del objeto del cliente MQTT
         self.mqttClient = mqtt.Client()
-        self.mqttClient.connect("3.133.119.117", 1883, 60)
+        self.mqttClient.connect("3.133.119.117", 8080, 60)
         self.mqttClient.subscribe("Bug_00/Report")
         self.mqttClient.subscribe("Bug_01/Report")
         self.mqttClient.on_message = self.on_message
         self.mqttClient.loop_start()
 
+        #Listas para contener datos temporales/actualizables
         self.sensorData1 = []
         self.sensorData2 = []
         self.currentAvg = []
         self.currentParamText = "Awaitng data"
             
 
-        # Set window size and position
-        screen_width = 810
-        screen_height = 700
+        #Valores del tamaño de pantalla y divisiones para las secciones internas
+        screenWidth = 810
+        screenHeight = 700
 
+        #Definicion del panel Izquierdo
+        leftPanel = tk.Frame(root, bg="lavender", width=screenWidth // 3, height=screenHeight, highlightbackground="black", highlightthickness=1)
+        leftPanel.pack(side=tk.LEFT, fill=tk.Y)
 
-        third_width = screen_width // 3
-        top_half_height = screen_height // 2
+        #Label y Combobox para Pais
+        label = tk.Label(leftPanel, text="Country", bg="lavender").grid(row=0, column=0, pady=10)
 
-        # Right panel
-        right_panel = tk.Frame(root, bg="lavender", width=third_width, height=screen_height, highlightbackground="black", highlightthickness=1)
-        right_panel.pack(side=tk.LEFT, fill=tk.Y)
-
-        
-        label = tk.Label(right_panel, text="Country", bg="lavender").grid(row=0, column=0, pady=10)
-
-        self.countryMenu = ttk.Combobox(right_panel, values=countries, width=5, justify='center', state='readonly')
+        self.countryMenu = ttk.Combobox(leftPanel, values=countries, width=5, justify='center', state='readonly')
         self.countryMenu.bind('<<ComboboxSelected>>', self.countryOption)
         self.countryMenu.set(country.get())
         self.countryMenu.grid(row=0, column=1, padx=10, pady=10, sticky='W')
 
-        label = tk.Label(right_panel, text="City", bg="lavender").grid(row=1, column=0, pady=10)
+        #Label y Combobox para ciudad
+        label = tk.Label(leftPanel, text="City", bg="lavender").grid(row=1, column=0, pady=10)
 
-        self.cityMenu = ttk.Combobox(right_panel, values=cities, width=15, justify='center', state='readonly')
+        self.cityMenu = ttk.Combobox(leftPanel, values=cities, width=15, justify='center', state='readonly')
         self.cityMenu.set(city.get())
         self.cityMenu.grid(row=1, column=1, padx=10, pady=10, sticky='W')
 
-        loadButton = tk.Button(right_panel, text="Copy Climate", command=self.onSelect)
+        #Boton para copiar clima
+        loadButton = tk.Button(leftPanel, text="Copy Climate", command=self.onSelect)
         loadButton.grid(row=2, column=0, columnspan=2, padx=10, pady=20)
 
-        self.targetParam = tk.Label(right_panel, text=paramet, bg="lavender")
+        #Label para parametros objetivo
+        self.targetParam = tk.Label(leftPanel, text=paramet, bg="lavender")
         self.targetParam.grid(row=3, column=0, columnspan=2, pady=30)
 
-        self.statusLabel = tk.Label(right_panel, text='', bg="lavender")
+        #Label para estado del programa
+        self.statusLabel = tk.Label(leftPanel, text='', bg="lavender")
         self.statusLabel.grid(row=4, column=0, columnspan=2)
 
-        self.pb = ttk.Progressbar(right_panel, orient= 'horizontal', length=150, mode='indeterminate')
+        #Barra de progreso
+        self.pb = ttk.Progressbar(leftPanel, orient= 'horizontal', length=150, mode='indeterminate')
         self.pb.grid(row=5, column=0, columnspan=2, padx=10, pady=10)
 
-        self.currentParam = tk.Label(right_panel, text=self.currentParamText, bg="lavender")
+        #Label para parametro actuales
+        self.currentParam = tk.Label(leftPanel, text=self.currentParamText, bg="lavender")
         self.currentParam.grid(row=6, column=0, columnspan=2, pady=30)
 
 
-        # Top left panel (Module 1)
-        module1_panel = tk.Frame(root, bg="lightgray", width=third_width * 2, height=screen_height / 4)
-        module1_panel.pack(side=tk.LEFT, fill=tk.BOTH, expand=False)
+        #Panel para modulo 1
+        module1Panel = tk.Frame(root, bg="lightgray", width=screenWidth // 3 * 2, height=screenHeight / 4)
+        module1Panel.pack(side=tk.LEFT, fill=tk.BOTH, expand=False)
         
 
-        # Add a title to the lateral panel
-        title_label = tk.Label(module1_panel, text="Module 1", bg="lightgray")
-        title_label.grid(row=0, column=0, columnspan=2, padx=5, pady=5, sticky='ns')
+        #Label del panel
+        titleLabel = tk.Label(module1Panel, text="Module 1", bg="lightgray")
+        titleLabel.grid(row=0, column=0, columnspan=2, padx=5, pady=5, sticky='ns')
 
-        # Create a Listbox to display the latest messages
-        self.tempListbox_1 = tk.Listbox(module1_panel, selectbackground="lightblue", justify='center', selectmode=tk.SINGLE)
+        #Listboxes para data de modulo 1
+        self.tempListbox_1 = tk.Listbox(module1Panel, selectbackground="lightblue", justify='center', selectmode=tk.SINGLE)
         self.tempListbox_1.grid(row=1, column=0, padx=5, pady=5)
 
-        self.humListbox_1 = tk.Listbox(module1_panel, selectbackground="lightblue", justify='center', selectmode=tk.SINGLE)
+        self.humListbox_1 = tk.Listbox(module1Panel, selectbackground="lightblue", justify='center', selectmode=tk.SINGLE)
         self.humListbox_1.grid(row=1, column=1, padx=5, pady=5)
 
-        tk.Label(module1_panel, text="Fan: ", bg="lightgray").grid(row=2, column=0, sticky='E', pady=(65, 30))
 
-        fanIndicator = tk.Canvas(module1_panel, width=25, height=25, bg="lightgray", highlightthickness=0)
+        #Labels e indicador para estado actual de actuadores en panel modulo 1
+        tk.Label(module1Panel, text="Fan: ", bg="lightgray").grid(row=2, column=0, sticky='E', pady=(65, 30))
+
+        fanIndicator = tk.Canvas(module1Panel, width=25, height=25, bg="lightgray", highlightthickness=0)
         fanIndicator.grid(row=2, column=1, padx=5, pady=(65, 30), sticky='W')
         self.fanStatus = fanIndicator.create_oval(3, 3, 25, 25, fill="gray35")
 
-        tk.Label(module1_panel, text="Humidifier: ", bg="lightgray").grid(row=3, column=0, sticky='E')
+        tk.Label(module1Panel, text="Humidifier: ", bg="lightgray").grid(row=3, column=0, sticky='E')
 
-        humIndicator = tk.Canvas(module1_panel, width=25, height=25, bg="lightgray", highlightthickness=0)
+        humIndicator = tk.Canvas(module1Panel, width=25, height=25, bg="lightgray", highlightthickness=0)
         humIndicator.grid(row=3, column=1, padx=5, pady=5, sticky='W')
         self.humStatus = humIndicator.create_oval(3, 3, 25, 25, fill="gray35")
 
 
         
-        # Top right panel (Module 2)
-        module2_panel = tk.Frame(root, bg="lightgray", width=third_width * 2, height=top_half_height // 2)
-        module2_panel.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+        #Panel para modulo 2
+        module2Panel = tk.Frame(root, bg="lightgray", width=screenWidth // 3 * 2, height=screenHeight // 2 // 2)
+        module2Panel.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 
-        # Add a title to the lateral panel
-        title_label = tk.Label(module2_panel, text="Module 2", bg="lightgray")
-        title_label.grid(row=0, column=0, columnspan=2, padx=5, pady=5, sticky='ns')
+        #Label del panel
+        titleLabel = tk.Label(module2Panel, text="Module 2", bg="lightgray")
+        titleLabel.grid(row=0, column=0, columnspan=2, padx=5, pady=5, sticky='ns')
 
-        # Create a Listbox to display the latest messages
-        self.tempListbox_2 = tk.Listbox(module2_panel, selectbackground="lightblue", justify='center', selectmode=tk.SINGLE)
+        #Listboxes para data de modulo 2
+        self.tempListbox_2 = tk.Listbox(module2Panel, selectbackground="lightblue", justify='center', selectmode=tk.SINGLE)
         self.tempListbox_2.grid(row=1, column=0, padx=5, pady=5)
 
-        self.humListbox_2 = tk.Listbox(module2_panel, selectbackground="lightblue", justify='center', selectmode=tk.SINGLE)
+        self.humListbox_2 = tk.Listbox(module2Panel, selectbackground="lightblue", justify='center', selectmode=tk.SINGLE)
         self.humListbox_2.grid(row=1, column=1, padx=5, pady=5)
 
-        tk.Label(module2_panel, text="Lighting: ", bg="lightgray").grid(row=2, column=0, sticky='E', pady=(65, 30))
+        #Labels e indicador para estado actual de actuadores en panel modulo 2
+        tk.Label(module2Panel, text="Lighting: ", bg="lightgray").grid(row=2, column=0, sticky='E', pady=(65, 30))
 
-        ledIndicator = tk.Canvas(module2_panel, width=25, height=25, bg="lightgray", highlightthickness=0)
+        ledIndicator = tk.Canvas(module2Panel, width=25, height=25, bg="lightgray", highlightthickness=0)
         ledIndicator.grid(row=2, column=1, padx=5, pady=(65, 30), sticky='W')
         self.ledStatus = ledIndicator.create_oval(0, 0, 25, 25, fill="gray35")
 
-        tk.Label(module2_panel, text="Air Conditioner: ", bg="lightgray").grid(row=3, column=0, sticky='E')
+        tk.Label(module2Panel, text="Air Conditioner: ", bg="lightgray").grid(row=3, column=0, sticky='E')
 
-        peltIndicator = tk.Canvas(module2_panel, width=25, height=25, bg="lightgray", highlightthickness=0)
+        peltIndicator = tk.Canvas(module2Panel, width=25, height=25, bg="lightgray", highlightthickness=0)
         peltIndicator.grid(row=3, column=1, padx=5, pady=5, sticky='W')
         self.peltStatus = peltIndicator.create_oval(0, 0, 25, 25, fill="gray35")
 
@@ -194,6 +204,7 @@ if __name__ == "__main__":
 
     root = tk.Tk()
 
+    #Configuracion del OWM
     config_dict = get_default_config_for_subscription_type('free')
     #Objeto del gestor del API
     owm = OWM('96ffa7534e55465cc7b0f714d3a9ba01', config_dict)
@@ -204,28 +215,30 @@ if __name__ == "__main__":
 
     countries = []
     cities = []
-               
+    
+    #Llenado de lista de paises desde el archivo json, ordenado, valor por defecto "MX"
     for i in data:
         if i['country'] not in countries:
             countries.append(i['country'])
-
     countries.sort()
-
     country = tk.StringVar(root)
     country.set('MX')
     city = tk.StringVar(root)
     
+    #Llenado de lista de ciudades del pais seleccionado actualmente, ordenado, valor por defecto "Hermosillo"
     for i in data:
         if i['country'] == country.get() and i['name'] not in cities:
             cities.append(i['name'])
     cities.sort()
     city.set('Hermosillo')
+
+    #Solicitud de datos de OWM y actualizado de etiquetas
     location = str(city.get())+","+str(country.get())
     observation = mgr.weather_at_place(location)
     w = observation.weather
     paramet = f"Target parameters \n Temperature {w.temperature('celsius')['temp']} | Humidity {w.humidity}"
 
-    
+    #Crea instancia de la clase y asigna el objeto a la variable gui
     gui = MyGUI(root)
     root.mainloop()
     
